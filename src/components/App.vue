6c58c4b726b91cdbeee004fde56930ee956762a2
okacchi-san <okacchi1979@gmail.com>
2021-01-15 15:02:27 +0900

    fix cors

--- src/components/App.vue
+++ src/components/App.vue
@@ -6,7 +6,7 @@
           <tr>
             <th>郵便番号</th>
             <td>
-              <input id="input" class="zipcode" type="text" name="zipcode" v-model="zipcode" placeholder="例)1110000">
+              <input id="zipcode" class="zipcode" type="text" name="zipcode" v-model="zipcode" placeholder="例)1110000">
               <button @click="onSearchclicked" id="seach" type="button">住所検索</button>
               <p id="error"></p>
             </td>
@@ -14,19 +14,19 @@
           <tr>
             <th>都道府県</th>
             <td>
-              <input id="address1" type="text" name="address1" value="">
+              <input id="address1" type="text" name="address1" :value="address1">
             </td>
           </tr>
           <tr>
             <th>市町村</th>
             <td>
-              <input id="address2" type="text" name="address2" value="">
+              <input id="address2" type="text" name="address2" :value="address2">
             </td>
           </tr>
           <tr>
             <th>町域</th>
             <td>
-              <input id="address3" type="text" name="address3" value="">
+              <input id="address3" type="text" name="address3" :value="address3">
             </td>
           </tr>
         </tbody>
@@ -40,22 +40,31 @@ export default{
   data: function(){
     return{
       zipcode: '',
-      addressData: {}
+      addressData: {},
+      address1: '',
+      address2: '',
+      address3: '',
     }
   },
   methods: {
     onSearchclicked(){
 
-    const response = fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${this.zipcode}`);
+    //Do: 入力文字のバリデーション
+    const pref = (this.zipcode).slice(0,3);
+    const city = (this.zipcode).slice(-4);
+
+    console.log(typeof(city))
+    const response = fetch(`https://madefor.github.io/postal-code-api/api/v1/${pref}/${city}.json`);
+   
 
     response.then((value) => {
       const json = value.json();
       json.then(v => {
-        this.addressData = v.results[0];
+        this.addressData = v.data[0];
         console.log(this.addressData)
-        address1.value = `${this.addressData['address1']} (${this.addressData['kana1']})`;
-        address2.value = `${this.addressData['address2']} (${this.addressData['kana2']})`;
-        address3.value = `${this.addressData['address3']} (${this.addressData['kana3']})`;
+        this.address1 = `${this.addressData.ja.prefecture}`;
+        this.address2 = `${this.addressData.ja.address1}`;
+        this.address3 = `${this.addressData.ja.address2}`;
       })
     })
     }
